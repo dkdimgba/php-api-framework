5bfe0e8839d1321f96080ac0145e0ffe3df725af
dkdimgba <dkdimgba@yahoo.com>
2017-02-03 19:08:29 +0100

    Corrected sample on test.php

--- test.php
+++ test.php
@@ -1,15 +1,6 @@
 <?php
 
-
-$data = 
-	'{
-		"value": "2348060923423",
-		"object": "phone"
-	}'; 
-
-
-
-$AUTH_TOKEN = "PJRcbDxdfBo49AwFvJDY-qZQmFQXL4MplqqfonfzV0Ag9SUtMoBIu096W8zdrI1kH9UJwVN-sw4S-q68ElP5yBYSFv_QO0O7_LStvqS-EjY6kBSTvRenRSViPqip-bf5dr2Gy2nx4ZqtC_a7GM4ekLjvB1vP0Pp_dXOKA8UIu3DkcTit6KNblM8JfSgZprs9ZK_mAM4LywJgW1f9L1CJ7HfbPLPXB0k21d39FVcs6ZBqhUX3Qvi3EgHN-1eKX6b8KvKYed3Kpw8wxJfPYqctGvdUWnGlIkq4yy6dkM3WNSeeLN3s0BSYx4pmPjDLYju8nIQSMsImetpCiTJOe3WpFQk1Vy4wk5ocPhrE4eBBNCutBpssdOa7DANRKNybvxz2Qg38aHlmGk0OR0qyUDVu8zEreRzGyCR7l0APhNbFBQ3WOCkX4QjfVJ-7alz25vSm6anYXk1vQVbJlnCaFBAt0mPl2S0aw9cu0NWY8eth2Z72hk1_DB8iFBb_YYyBAt8A4XSle9QBKlUC-YH27zXi3Q";
+$AUTH_TOKEN = "";
 
 // initialie the curl function
 $ch = curl_init();
